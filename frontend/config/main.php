a6a38ecf45a3e4eb91f9fc3c9c832fb046bfac54
toanlb <toanlb@live.com>
2025-04-22 19:35:43 +0700

    text

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,7 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
-    'name' => 'Zplus Kiot POS',
+    'name' => 'Zplus Kiot',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
